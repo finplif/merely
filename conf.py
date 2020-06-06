35cd5861daf383532fa2495d35292959862f3b29
finplif <42929440+finplif@users.noreply.github.com>
2020-06-06 12:07:21 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,1 +1,5 @@
 TOKEN = "1258741909:AAHbh8TLkhX4zaIjAmRquAKIuvNUBFn60kA"
+PROXY = {
+    'https': 'socks5://116603239:BiRRKqh2@ss-01.s5.ynvv.cc:999',
+    'socks5': 'socks5://116603239:BiRRKqh2@ss-01.s5.ynvv.cc:999'
+}
